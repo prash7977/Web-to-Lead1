5c5c146562561e637077e1a2482bffe5dd901979
prashant <prashant.nimase00@gmail.com>
2024-11-27 01:14:05 +0530

    capcha added1

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // system.debug(UserInfo.getLocale()); --> to get date info (en_US)
 // captcha link - https://www.google.com/recaptcha/admin/create
 
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate.value", inputdate.value);
-
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
-    outputdate.value = formattedDate;
+let capthachecked = false;
+if(capthachecked){
+    function beforesubmit(event){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("inputdate.value", inputdate.value);
+    
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+        outputdate.value = formattedDate;
+    } else {
+        alert("Please check the reCAPTCHAA box to submit the lead");
+        event.preventDefault();
+    }
 }
 
+
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
+function capthasucess(){
+    capthachecked = true;
+}
