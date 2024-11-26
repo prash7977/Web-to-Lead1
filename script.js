a21df705fccf841993b5248083239243008dbea9
prashant <prashant.nimase00@gmail.com>
2024-11-27 00:59:51 +0530

    capcha

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // system.debug(UserInfo.getLocale()); --> to get date info (en_US)
+// captcha link - https://www.google.com/recaptcha/admin/create
 
 function beforesubmit(){
     let outputdate = document.querySelector(".outputdate");
@@ -8,3 +9,5 @@ function beforesubmit(){
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value = formattedDate;
 }
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
